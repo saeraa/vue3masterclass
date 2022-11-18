4cb5023127034823b06bebd779b744f276fbdfc5
Sara <saeraa@gmail.com>
2022-11-18 15:26:23 +0100

    adding data.json

---
+++ src/components/PageHome.vue
@@ -0,0 +1,36 @@
+<template>
+	<h1>Page Home</h1>
+	<div v-for="thread in threads" :key="thread">
+		<h2>{{ thread.title }}</h2>
+		<div v-for="postId in thread.posts" :key="postId">
+			<p>
+				{{ userById(postById(postId).userId).name }}
+			</p>
+			<p>{{ postById(postId).text }}</p>
+		</div>
+	</div>
+	<h3></h3>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+export default {
+	data() {
+		return {
+			threads: sourceData.threads,
+			posts: sourceData.posts,
+			users: sourceData.users
+		};
+	},
+	methods: {
+		postById(postId) {
+			return this.posts.find((p) => p.id === postId);
+		},
+		userById(userId) {
+			return this.users.find((p) => p.id === userId);
+		}
+	}
+};
+</script>
+
+<style scoped></style>
